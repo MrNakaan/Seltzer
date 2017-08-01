af32acd9a40e9fbd717905c25b4dced310fa9c33
Caleb Hall <caleb.a.hall@gmail.com>
2017-07-31 22:39:49 -0500

    Nightly

---
+++ seltzer-site/src/main/java/tech/seltzer/BasePage.java
@@ -0,0 +1,20 @@
+package tech.seltzer;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class BasePage extends WebPage {
+	private static final long serialVersionUID = 1L;
+
+	public BasePage(final PageParameters parameters) {
+		super(parameters);
+    }
+	
+	@Override
+	protected void onInitialize() {
+		super.onInitialize();
+		
+		add(new Header("headerPanel"));
+		add(new Footer("footerPanel"));
+	}
+}
---
+++ seltzer-site/src/main/java/tech/seltzer/Index.java
@@ -0,0 +1,12 @@
+package tech.seltzer;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class Index extends BasePage {
+	private static final long serialVersionUID = 7146561197795644314L;
+
+	public Index(PageParameters parameters) {
+		super(parameters);
+	}
+
+}
