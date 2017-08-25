0c2a86ab1618c9ac9873023c5c3ed7d924925b30
Caleb Hall <caleb.a.hall@gmail.com>
2017-08-25 01:03:30 -0500

    Just a little cleanup

--- copy_bundles.bat
+++
@@ -1,5 +0,0 @@
-rem I have a folder called "bnd" at the same level as my Git repos that I store my bundles in.
-rem This script copies my seltzer-cr and seltzer-util bundle to a folder within that.
-mkdir ..\bnd\Seltzer
-copy ".\seltzer-parent\seltzer-cr\target\seltzer-cr-*.jar" "..\bnd\Seltzer"
-copy ".\seltzer-parent\seltzer-util\target\seltzer-util-*.jar" "..\bnd\Seltzer"
