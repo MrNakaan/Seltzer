8982f0442ea95ac579e0cfe2bb8802c087a40715
Caleb Hall <caleb.a.hall@gmail.com>
2018-04-21 23:37:49 -0500

    Formatting and tidying

--- seltzer-parent/seltzer-core/src/main/java/tech/seltzer/core/processor/SelectorProcessor.java
+++ seltzer-parent/seltzer-core/src/main/java/tech/seltzer/core/processor/SelectorProcessor.java
@@ -4,7 +4,6 @@ import java.util.UUID;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.NoSuchElementException;
@@ -28,8 +27,8 @@ import tech.seltzer.objects.response.Response;
 import tech.seltzer.objects.response.SingleResultResponse;
 
 /**
- * Processor for <code>SelectorCommandData</code> and all subclasses. 
- * Delegates to <code>MultiResultSelectorProcessor</code> as appropriate.
+ * Processor for <code>SelectorCommandData</code> and all subclasses. Delegates
+ * to <code>MultiResultSelectorProcessor</code> as appropriate.
  */
 public class SelectorProcessor {
 	static Logger logger = LogManager.getLogger(SelectorProcessor.class);
@@ -65,10 +64,10 @@ public class SelectorProcessor {
 					case SEND_KEYS:
 						response = sendKeys(driver, (SendKeysCommandData) command);
 						break;
-//					Removed for now since only Edge supports it
-//					case SCREENSHOT_ELEMENT:
-//						response = takeScreenshot(driver, command);
-//						break;
+					// Removed for now since only Edge supports it
+					// case SCREENSHOT_ELEMENT:
+					// response = takeScreenshot(driver, command);
+					// break;
 					default:
 						response = new Response(command.getId(), false);
 						break;
@@ -106,7 +105,8 @@ public class SelectorProcessor {
 		return response;
 	}
 
-	private static SingleResultResponse count(WebDriver driver, SelectorCommandData command) throws NoSuchElementException {
+	private static SingleResultResponse count(WebDriver driver, SelectorCommandData command)
+			throws NoSuchElementException {
 		SingleResultResponse response = new SingleResultResponse(command.getId());
 
 		Integer size = BaseProcessor.getElements(command, command.getSelector()).size();
@@ -118,12 +118,12 @@ public class SelectorProcessor {
 
 	private static Response delete(WebDriver driver, SelectorCommandData command) {
 		Response response = new Response(command.getId(), false);
-		if(driver instanceof JavascriptExecutor) {
+		if (driver instanceof JavascriptExecutor) {
 			Selector convertedSelector = convert(command.getId(), command.getSelector());
 
 			if (convertedSelector.getType() == SelectorType.XPATH) {
 				String selector = convertedSelector.getPath().replace("\"", "\\\"");
-	
+
 				StringBuilder removeScript = new StringBuilder();
 				removeScript.append(Messages.getString("SelectorProcessor.js.xpath1"));
 				removeScript.append(Messages.getString("SelectorProcessor.js.xpath2"));
@@ -135,10 +135,10 @@ public class SelectorProcessor {
 				removeScript.append(Messages.getString("SelectorProcessor.js.xpath7"));
 				removeScript.append(Messages.getString("SelectorProcessor.js.xpath8"));
 				removeScript.append(Messages.getString("SelectorProcessor.js.xpath7"));
-	
+
 				JavascriptExecutor executor = (JavascriptExecutor) driver;
 				executor.executeScript(removeScript.toString());
-	
+
 				response.setSuccess(true);
 			} else if (convertedSelector.getType() == SelectorType.CSS_SELECTOR) {
 				StringBuilder removeScript = new StringBuilder();
@@ -148,10 +148,10 @@ public class SelectorProcessor {
 				removeScript.append(Messages.getString("SelectorProcessor.js.css3"));
 				removeScript.append(Messages.getString("SelectorProcessor.js.css4"));
 				removeScript.append(Messages.getString("SelectorProcessor.js.css5"));
-	
+
 				JavascriptExecutor executor = (JavascriptExecutor) driver;
 				executor.executeScript(removeScript.toString());
-	
+
 				response.setSuccess(true);
 			}
 		}
@@ -179,7 +179,7 @@ public class SelectorProcessor {
 
 		return response;
 	}
-	
+
 	private static Response sendKey(WebDriver driver, SendKeyCommandData command) {
 		Response response = new Response(command.getId(), true);
 
@@ -198,81 +198,81 @@ public class SelectorProcessor {
 
 		return response;
 	}
-	
+
 	@SuppressWarnings("unused")
-	private static Response takeScreenshot(WebDriver driver, SelectorCommandData command) throws WebDriverException, Exception {
+	private static Response takeScreenshot(WebDriver driver, SelectorCommandData command)
+			throws WebDriverException, Exception {
 		WebElement element = BaseProcessor.getElements(command, command.getSelector()).get(0);
-		
+
 		String screenshot = ((TakesScreenshot) element).getScreenshotAs(OutputType.BASE64);
 
 		SingleResultResponse response = new SingleResultResponse(command.getId(), true);
 		response.setResult(screenshot);
-		
+
 		return response;
 	}
-	
+
 	private static Selector convert(UUID id, Selector selector) {
 		if (selector.getType() == SelectorType.INDEX) {
 			selector = SeltzerSession.findSession(id).getCachedSelectionSelector(Integer.valueOf(selector.getPath()));
 		}
-		
-		if (selector.getType() == SelectorType.XPATH || selector.getType() == SelectorType.CSS_SELECTOR) {
+
+		switch (selector.getType()) {
+		case XPATH:
+		case CSS_SELECTOR:
 			return selector;
-		} else {
-			switch (selector.getType()) {
-			case TAG_NAME:
-			case ID:
-			case LINK_TEXT:
-			case PARTIAL_LINK_TEXT:
-			case NAME:
-				return convertToXpath(selector);
-			case CLASS_NAME:
-				return convertToCssSelector(selector);
-			default:
-				return new Selector();
-			}
+		case TAG_NAME:
+		case ID:
+		case LINK_TEXT:
+		case PARTIAL_LINK_TEXT:
+		case NAME:
+			return convertToXpath(selector);
+		case CLASS_NAME:
+			return convertToCssSelector(selector);
+		default:
+			return new Selector();
 		}
 	}
-	
+
 	private static Selector convertToXpath(Selector selector) {
 		Selector convertedSelector = new Selector(SelectorType.XPATH, "");
-		
+
 		switch (selector.getType()) {
-			case TAG_NAME:
-				convertedSelector.setPath("//" + selector.getPath());
-				break;
-			case ID:
-				convertedSelector.setPath("//*[@id='" + selector.getPath() + "']");
-				break;
-			case LINK_TEXT:
-				convertedSelector.setPath("//a[text()='" + selector.getPath() + "']");
-				break;
-			case PARTIAL_LINK_TEXT:
-				convertedSelector.setPath("//a[contains(text(),'" + selector.getPath() + "')]");
-				break;
-			case NAME:
-				convertedSelector.setPath("//*[@name='" + selector.getPath() + "']");
-				break;
-			default:
-				convertedSelector.setType(SelectorType.NONE);
-				break;
+		case TAG_NAME:
+			convertedSelector.setPath("//" + selector.getPath());
+			break;
+		case ID:
+			convertedSelector.setPath("//*[@id='" + selector.getPath() + "']");
+			break;
+		case LINK_TEXT:
+			convertedSelector.setPath("//a[text()='" + selector.getPath() + "']");
+			break;
+		case PARTIAL_LINK_TEXT:
+			convertedSelector.setPath("//a[contains(text(),'" + selector.getPath() + "')]");
+			break;
+		case NAME:
+			convertedSelector.setPath("//*[@name='" + selector.getPath() + "']");
+			break;
+		default:
+			convertedSelector.setType(SelectorType.NONE);
+			break;
 		}
-		
+
 		return convertedSelector;
 	}
-	
+
 	private static Selector convertToCssSelector(Selector selector) {
 		Selector convertedSelector = new Selector(SelectorType.CSS_SELECTOR, "");
-		
+
 		switch (selector.getType()) {
-			case CLASS_NAME:
-				convertedSelector.setPath("." + selector.getPath());
-				break;
-			default:
-				convertedSelector.setType(SelectorType.NONE);
-				break;
+		case CLASS_NAME:
+			convertedSelector.setPath("." + selector.getPath());
+			break;
+		default:
+			convertedSelector.setType(SelectorType.NONE);
+			break;
 		}
-		
+
 		return convertedSelector;
 	}
 }
